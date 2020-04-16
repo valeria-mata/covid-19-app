2a8b66fe1a1f1e40446a34f5843666f6f9f038da
Valeria Mata <valeria.mata@axity.com>
2020-04-16 13:01:30 -0500

    Creación de pagína de slider

---
+++ src/app/constants/slides.ts
@@ -0,0 +1,10 @@
+import { Slides } from '../models/catalogs';
+
+export const slidesInfo: Slides[] = [
+    { image:'', text:'¡Hola! Antes de que comiences a utilizar nuestra App queremos decirte algunas cosas...' },
+    { image:'', text:'Queremos ayudar, es por ello que te pediremos nos proporciones algunos datos tuyos que utilizaremos sólo con fines estadísticos.' },
+    { image:'', text:'La App fue creada para obtener información sobre las personas que ya han sido diagnosticadas con COVID-19...' },
+    { image:'', text:'Por esta razón, te pediremos tu nombre completo, tu número de celular, tu correo electrónico y que tomes una fotografía de tu diagnóstico.' },
+    { image:'', text:'Una vez que confirmes el envío de tu diagnóstico, se enviará un correo electrónico a la Secretaría de Salud con los datos que nos compartiste anteriormente.' },
+    { image:'', text:'Esperamos que muy pronto podemos regresar a nuestra vida cotidiana, pero por lo pronto, te invitamos a seguir cuidando tu salud con las medidas pertinentes.' }
+]
